0a444df66c85edaaa0efa073ba8d709a083975c7
Gada Suriya Narayana <116232067+SuriyaNarayana6765@users.noreply.github.com>
2023-07-05 17:04:58 +0530

    Add files via upload

--- Workbox-webpack-plugin/index.js
+++ Workbox-webpack-plugin/index.js
@@ -1,15 +1,17 @@
+"use strict";
 /*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Tobias Koppers @sokra
+  Copyright 2018 Google LLC
+
+  Use of this source code is governed by an MIT-style
+  license that can be found in the LICENSE file or at
+  https://opensource.org/licenses/MIT.
 */
-exports.Source = require("./Source");
-
-exports.RawSource = require("./RawSource");
-exports.OriginalSource = require("./OriginalSource");
-exports.SourceMapSource = require("./SourceMapSource");
-exports.LineToLineMappedSource = require("./LineToLineMappedSource");
-
-exports.CachedSource = require("./CachedSource");
-exports.ConcatSource = require("./ConcatSource");
-exports.ReplaceSource = require("./ReplaceSource");
-exports.PrefixSource = require("./PrefixSource");
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.InjectManifest = exports.GenerateSW = void 0;
+const generate_sw_1 = require("./generate-sw");
+Object.defineProperty(exports, "GenerateSW", { enumerable: true, get: function () { return generate_sw_1.GenerateSW; } });
+const inject_manifest_1 = require("./inject-manifest");
+Object.defineProperty(exports, "InjectManifest", { enumerable: true, get: function () { return inject_manifest_1.InjectManifest; } });
+// TODO: remove this in v7.
+// See https://github.com/GoogleChrome/workbox/issues/3033
+exports.default = { GenerateSW: generate_sw_1.GenerateSW, InjectManifest: inject_manifest_1.InjectManifest };
